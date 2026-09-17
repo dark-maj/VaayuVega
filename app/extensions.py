e8a243c43d79fd6a8ec22136125e39559cc09ab3
dark-maj <jaahnavi005@gmail.com>
2026-09-17 18:29:54 +0530

    files:admin_views,extensions changes

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
- 
+from flask_admin import Admin
+from flask_admin.theme import Bootstrap4Theme
+
 db=SQLAlchemy()
 login=LoginManager()
+admin=Admin(name="Vaayu Vega Admin", theme=Bootstrap4Theme())
